5a077e2021b5915f3630d86a88e6201daf5e9ead
Agustin de Arteaga <agustin.dearteaga@lightit.io>
2025-12-30 11:24:24 -0300

    feat: update doctor endpoint

--- src/Doctors/App/Requests/UpsertDoctorRequest.php
+++ src/Doctors/App/Requests/UpsertDoctorRequest.php
@@ -5,7 +5,21 @@ declare(strict_types=1);
 namespace Lightit\Doctors\App\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Lightit\Doctors\Domain\DataTransferObjects\DoctorDto;
 
 class UpsertDoctorRequest extends FormRequest
 {
+    public const string NAME = 'name';
+
+    public function rules(): array
+    {
+        return [
+            self::NAME => ['required', 'string', 'max:50'],
+        ];
+    }
+
+    public function toDto(): DoctorDto
+    {
+        return new DoctorDto(name: $this->string(self::NAME)->toString());
+    }
 }
