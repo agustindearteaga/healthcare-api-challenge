7d43f0c49d380b0fa4d6e3d431d8cd6f7de9454c
Agustin de Arteaga <agustin.dearteaga@lightit.io>
2026-01-16 17:25:36 -0300

    feat: clinic model and migrations

--- src/Doctors/Domain/Models/Doctor.php
+++ src/Doctors/Domain/Models/Doctor.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Lightit\Doctors\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Lightit\Clinics\Domain\Models\Clinic;
 
 /**
  * @property int $id
@@ -23,4 +25,12 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     protected $guarded = ['id'];
+
+    /**
+     * @return BelongsToMany<Clinic, $this>
+     */
+    public function clinics(): BelongsToMany
+    {
+        return $this->belongsToMany(Clinic::class);
+    }
 }
