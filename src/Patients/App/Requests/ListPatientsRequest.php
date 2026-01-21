fb76176fd82aeab1d8d3927f604c263b205ea973
Agustin de Arteaga <agustin.dearteaga@lightit.io>
2026-01-21 16:33:18 -0300

    refactor: magic numbers to const on list patient request

--- src/Patients/App/Requests/ListPatientsRequest.php
+++ src/Patients/App/Requests/ListPatientsRequest.php
@@ -12,6 +12,10 @@ final class ListPatientsRequest extends FormRequest
 
     public const string PER_PAGE = 'per_page';
 
+    public const int DEFAULT_ITEMS_PER_PAGE = 15;
+
+    public const int STARTING_PAGE = 1;
+
     public function rules(): array
     {
         return [
@@ -22,11 +26,11 @@ final class ListPatientsRequest extends FormRequest
 
     public function pageNumber(): int
     {
-        return $this->integer(self::PAGE, 1);
+        return $this->integer(self::PAGE, self::STARTING_PAGE);
     }
 
     public function itemsPerPage(): int
     {
-        return $this->integer(self::PER_PAGE, 15);
+        return $this->integer(self::PER_PAGE, self::DEFAULT_ITEMS_PER_PAGE);
     }
 }
